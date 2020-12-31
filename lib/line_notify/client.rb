7a7f19c02ba0440b194bdb7e43db05479e50b096
Unayung Chen <unayung@gmail.com>
2020-12-31 10:42:17 +0800

    add revoke

--- lib/line_notify/client.rb
+++ lib/line_notify/client.rb
@@ -2,24 +2,34 @@ require 'line_notify/version'
 require 'net/https'
 
 module LineNotify
+  # line notify
   class Client
-    URI = URI.parse("https://notify-api.line.me/api/notify")
+    NOTIFY_URI = URI.parse('https://notify-api.line.me/api/notify')
+    REVOKE_URI = URI.parse('https://notify-api.line.me/api/revoke')
 
     def initialize(access_token)
       @access_token = (access_token || ENV['LINE_ACCESS_TOKEN'])
     end
 
     def ping(options)
-      request = create_request(options)
-      Net::HTTP.start(URI.hostname, URI.port, use_ssl: URI.scheme == "https") do |req|
+      request = create_request(options, NOTIFY_URI)
+      Net::HTTP.start(NOTIFY_URI.hostname, NOTIFY_URI.port, use_ssl: NOTIFY_URI.scheme == 'https') do |req|
+        req.request(request)
+      end
+    end
+
+    def revoke(options)
+      request = create_request(options, REVOKE_URI)
+      Net::HTTP.start(REVOKE_URI.hostname, REVOKE_URI.port, use_ssl: REVOKE_URI.scheme == 'https') do |req|
         req.request(request)
       end
     end
 
     private
-    def create_request(options)
-      request = Net::HTTP::Post.new(URI)
-      request["Authorization"] = "Bearer #{@access_token}"
+
+    def create_request(options, uri)
+      request = Net::HTTP::Post.new(uri)
+      request['Authorization'] = "Bearer #{@access_token}"
       request.set_form_data(options)
       request
     end
